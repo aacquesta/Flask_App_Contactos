e1c3144d9c65682337d739f2136888764fe313e3
Alejandro desde Trabajo <aacquesta@citedef.gob.ar>
2020-02-21 11:31:27 -0300

    Subo conectado con mysql

--- App.py
+++ App.py
@@ -1,5 +1,28 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+app.config['MYSQL_HOST']= 'localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= 'localhost'
+app.config['MYSQL_DB']= 'flaskcontacts'
+mysql = MySQL(app)
+
+@app.route('/')
+def Index():
+    return "Hello World"
+
+@app.route('/add_contact')
+def add_contact():
+    return "add contact"
+
+@app.route('/edit')#falta parametro
+def edit_contact():
+    return "edit contact"
+
+@app.route('/delete')#falta parametro
+def delete_contact():
+    return "delete contact"
+
 
 if __name__ == '__main__':
     app.run(port = 3000, debug= True) 
